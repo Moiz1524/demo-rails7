b222da59b83cc793e3e04afa0b383ba6f8aa3c6c
Moiz Ali <amoiz4142@gmail.com>
2022-05-13 00:28:04 +0500

    Add sidekiq as queue adapter

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module DemoRails7
     config.load_defaults 7.0
 
     config.active_storage.variant_processor = :mini_magick
+    config.active_job.queue_adapter = :sidekiq
 
     # Configuration for the application, engines, and railties goes here.
     #
