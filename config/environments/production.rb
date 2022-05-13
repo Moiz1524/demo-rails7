af43651e5dd2164bfe22d8fd3e3722372600fcd8
Moiz Ali <amoiz4142@gmail.com>
2022-05-13 19:14:53 +0500

    Minor change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,8 @@ Rails.application.configure do
     :password => Rails.application.credentials.dig(:mailgun, :password)
   }
   config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
   
   config.action_mailer.perform_caching = false
 
