b304dee6512c50f11909ad00e549be95c5f5f801
Moiz Ali <amoiz4142@gmail.com>
2022-05-16 20:39:58 +0500

    Used key instead of string

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    :authentication => 'plain',
+    :authentication => :plain,
     :address => "smtp.mailgun.org",
     :port => '587',
     :domain => Rails.application.credentials.dig(:mailgun, :domain),
