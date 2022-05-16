319516dc9c28c57cdf6b7db61d002481379fa779
Moiz Ali <amoiz4142@gmail.com>
2022-05-16 21:03:53 +0500

    Don't raise delivery errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
   
   # Use Mailgun SMTP credentials for sending emails
   config.action_mailer.delivery_method = :smtp
